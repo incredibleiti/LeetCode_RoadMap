3ea54a8de85276fe48cb185a5e400921fae57923
Iti Tyagi <intechworx@gmail.com>
2025-03-05 16:14:45 +0100

    Update ValidAnagram.cpp

--- TopInterview/Easy/ValidAnagram.cpp
+++ TopInterview/Easy/ValidAnagram.cpp
@@ -32,3 +32,26 @@ public:
         
     }
 };
+
+=================== Using hashMap =======================
+
+
+ class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+
+        unordered_map<char, int> sMap;
+        for(char c: s) {
+            sMap[c]++;
+        }
+
+        for(char c : t) {
+            if(sMap[c]<=0) {
+                return false;
+            }
+            sMap[c]--;
+        }
+        return true;
+    }
+};
