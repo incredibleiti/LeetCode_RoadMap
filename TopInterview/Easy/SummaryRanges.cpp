f138a4edd98bcf23bcbdcb8afe5107abc4a32bde
Iti Tyagi <intechworx@gmail.com>
2025-03-07 16:13:57 +0100

    Update SummaryRanges.cpp

--- TopInterview/Easy/SummaryRanges.cpp
+++ TopInterview/Easy/SummaryRanges.cpp
@@ -66,3 +66,19 @@ public:
     }
 };
 
+
+
+====================== Fails for longer range data so fixed below ==============================
+
+ vector<string> summaryRanges(vector<int> &nums) {
+        vector<string> ans;
+        if (!nums.size()) return ans;
+        int f = nums[0], s = nums[0], len = nums.size();
+        for (int i = 1; i < len; i++)
+            if (s + 1 == nums[i])
+                s = nums[i];
+            else
+                f == s ? ans.push_back(to_string(f)) : ans.push_back(to_string(f) + "->" + to_string(s)), f = s = nums[i];
+        f == s ? ans.push_back(to_string(f)) : ans.push_back(to_string(f) + "->" + to_string(s));
+        return ans;
+    }
