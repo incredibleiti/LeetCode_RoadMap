d3ea231c43c0bb816d4c16d363c7b04e87375f84
Iti Tyagi <intechworx@gmail.com>
2025-03-09 10:07:26 +0100

    Create ProductofArrayExceptSelf.cpp

---
+++ TopInterview/Medium/ProductofArrayExceptSelf.cpp
@@ -0,0 +1,34 @@
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+================ Brut force =======================
+  class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+         vector<int> result;
+        for(int i=0; i<nums.size(); ++i) {
+            int val = 1;
+            for(int j=0; j<nums.size(); ++j) {
+                if(i!=j) {
+                    val*=nums[j];
+                }
+            }
+            result.push_back(val);
+        }
+        return result;
+        }
+};
