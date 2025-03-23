8873e00185214c9011be00cbd44180b7c30234cd
Iti Tyagi <intechworx@gmail.com>
2025-03-23 17:27:18 +0100

    Update JumpII.cpp

--- TopInterview/Medium/JumpII.cpp
+++ TopInterview/Medium/JumpII.cpp
@@ -38,3 +38,24 @@ public:
 };
 
 which failed at nums = [0]
+
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jump = 0;
+        int end = 0;
+        int farthest = 0;
+
+        for(int i=0; i<nums.size()-1; ++i) {
+            farthest = max(farthest, i+nums[i]);
+
+            if(i == end) //reached the point of jump
+            {
+                jump++;
+                end = farthest;
+            }
+        }
+        return jump;
+    }
+};
