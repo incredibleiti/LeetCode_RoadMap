bd757da7ef2bf5d8ecf511aaf07116a374fbba5a
Iti Tyagi <intechworx@gmail.com>
2025-03-31 23:03:47 +0200

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -16,3 +16,30 @@ Example 3:
 
 Input: nums = [1,3,5,6], target = 7
 Output: 4
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+         int low=0;
+        int high=nums.size();
+        int mid;
+        if(target>nums[high-1]){
+            return high;
+        }
+        while(low<=high){
+              mid=(low+high)/2;
+            if(nums[mid]==target){  
+                return mid;
+            }
+          
+            if(target<nums[mid]){     
+            high=mid-1;    
+            }else{
+            low=mid+1;        
+            }
+          
+        }
+         return  low;   
+    
+    }
+};
